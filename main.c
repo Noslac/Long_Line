e704dbbafa08cab7c15ad733cd3278a364bfb65d
Ndifon Desmond Yong <yongdesmond23@gmail.com>
2017-05-09 14:45:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-char board[4][4];
-int index;
+#include<conio.h> // This includes functions clrscr(); that clears screen instead of the widely used system
+// functions, implies program is for cross platforms
+char board[4][4]; /* This is the board */
+
 int main ()
 {
 	return 0;
